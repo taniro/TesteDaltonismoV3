c1946a62680f7204d45bdf343f4dab64cd9d44a6
tanirocr@gmail.com <alakazamxdgithub12>
2018-08-07 23:01:33 -0300

    Initial commit

---
+++ app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/MainActivity.java
@@ -0,0 +1,13 @@
+package br.ufrn.eaj.tads.testedaltonismo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
