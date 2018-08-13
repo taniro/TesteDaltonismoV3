c68a9795bebdbc3e557e8ba6e8d8f31509189053
tanirocr@gmail.com <alakazamxdgithub12>
2018-08-12 21:53:42 -0300

    resposta

--- app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/MainActivity.java
+++ app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/MainActivity.java
@@ -1,13 +1,113 @@
 package br.ufrn.eaj.tads.testedaltonismo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int TESTE_HUM = 55;
+    private static final int TESTE_DOIS = 56;
+    private static final int TESTE_TRES = 57;
+
+    String r1, r2, r3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i("AULA", "Invocou o metodo onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i("AULA", "Invocou o metodo onRestoreInstanceState");
+    }
+
+    public void metodoCliqueTesteHum(View v){
+
+        Intent i = new Intent(this, TesteActivity.class);
+        i.putExtra("teste", 1);
+
+        startActivityForResult(i, TESTE_HUM);
+    }
+
+    public void metodoCliqueTesteDois(View v){
+
+        Intent i = new Intent(this, TesteActivity.class);
+        i.putExtra("teste", 2);
+
+        startActivityForResult(i, TESTE_DOIS);
+    }
+
+    public void metodoCliqueTesteTres(View v){
+
+        Intent i = new Intent(this, TesteActivity.class);
+        i.putExtra("teste", 3);
+
+        startActivityForResult(i, TESTE_TRES);
+    }
+
+    public void verificar(View v){
+        TextView tv = findViewById(R.id.resultado_teste);
+
+        if(r1 == null || r2 == null || r3 == null){
+            Toast.makeText(this, "Responda os testes.", Toast.LENGTH_SHORT).show();
+        }else if (r1.equals("2") && r2.equals("29") && r3.equals("74")){
+            tv.setText("Tudo Ok!");
+        }else{
+            tv.setText("Procure o médico");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(data == null || resultCode == RESULT_CANCELED){
+            return;
+        }
+
+        switch (requestCode){
+            case TESTE_HUM:{
+                if (resultCode == RESULT_OK){
+                    TextView tv = findViewById(R.id.resposta1);
+
+                    String resposta = data.getStringExtra("resposta");
+                    tv.setText(resposta);
+                    r1 = resposta;
+                    break;
+                }
+            }case TESTE_DOIS:{
+                if (resultCode == RESULT_OK){
+                    TextView tv = findViewById(R.id.resposta2);
+
+                    String resposta = data.getStringExtra("resposta");
+                    tv.setText(resposta);
+                    r2 = resposta;
+                    break;
+                }
+            }case TESTE_TRES:{
+                if (resultCode == RESULT_OK){
+                    TextView tv = findViewById(R.id.resposta3);
+
+                    String resposta = data.getStringExtra("resposta");
+                    tv.setText(resposta);
+                    r3 = resposta;
+                    break;
+                }
+            }
+        }
+
     }
 }
--- app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/TesteActivity.java
+++ app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/TesteActivity.java
@@ -1,13 +1,68 @@
 package br.ufrn.eaj.tads.testedaltonismo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class TesteActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teste);
+
+        try {
+            int teste_id = getIntent().getIntExtra("teste", 0);
+
+            ImageView iv = findViewById(R.id.imageView);
+
+            switch (teste_id){
+                case 1:{
+                    iv.setImageResource(R.drawable.teste1);
+                    break;
+                }case 2:{
+                    iv.setImageResource(R.drawable.teste2);
+                    break;
+                }case 3:{
+                    iv.setImageResource(R.drawable.teste3);
+                    break;
+                }
+            }
+
+        }catch (NullPointerException e){
+            Toast.makeText(this, "Erro.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void cliqueOk(View v){
+
+        EditText et = findViewById(R.id.editText);
+
+
+        if (!et.getText().toString().isEmpty()){
+
+            String resposta = et.getText().toString();
+
+            Intent i = new Intent();
+            i.putExtra("resposta", resposta);
+
+            setResult(RESULT_OK, i);
+
+            finish();
+
+        }else{
+            Toast.makeText(this, "Preecha um valor.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void cliqueCancel(View v){
+        setResult(RESULT_CANCELED);
+        finish();
     }
 }
